7e98c67ef65a0333577116397aad2b3eb03795ea
faraz7321 <81792548+faraz7321@users.noreply.github.com>
2021-04-24 13:09:07 +0500

    Task1 2 functions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# PD9_OOP_144_156
-String Manipulation
+## Getting Started
+
+Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+## Dependency Management
+
+The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
---
+++ src/Task1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Enter a String: ");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("This string contains " + wordCounter(s) + " words");
+        System.out.println("This string contains " + vowelCounter(s) + " vowels");
+        scanner.close();
+
+    }
+
+    public static int wordCounter(String s) {
+        int count = 1;
+        if (s == "") {
+            return 0;
+        }
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int vowelCounter(String s) {
+        int count = 0;
+        if (s == "") {
+            return 0;
+        }
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
